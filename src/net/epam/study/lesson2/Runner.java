ce79b5a7ce64efada8b2e84250063441ed5745af
Kirill Mordas <kirill.mordas@gmail.com>
2021-02-02 18:14:59 +0300

    Git naming fix

---
+++ src/net/epam/study/lesson2/Runner.java
@@ -0,0 +1,15 @@
+package net.epam.study.lesson2;
+
+import java.io.IOException;
+
+public class Runner {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Lowest price element: ");
+        System.out.println(HouseholdAppliances.getMinPriceElement(HouseholdAppliances.fileReader(),
+                HouseholdAppliances.getMinPrice(HouseholdAppliances.textParser(HouseholdAppliances.fileReader()))) + "\n");
+        System.out.println("Kettles: ");
+        for (int i = 0; i<HouseholdAppliances.getKettles(HouseholdAppliances.fileReader()).size(); i++){
+            System.out.println(HouseholdAppliances.getKettles(HouseholdAppliances.fileReader()).get(i));
+        }
+    }
+}
